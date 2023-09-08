2d5a6b02a6b1fbbd8581cb4cb6b7f1be1a3449a2
1ikuszak <luke.glica@gmail.com>
2023-09-08 18:23:19 +0200

    Moved from previous project

---
+++ SoundMeter/Styles/LargeLabelControl.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace SoundMeter;
+
+public class LargeLabelControl : TemplatedControl
+{
+    public static readonly StyledProperty<string> LargeTextProperty = AvaloniaProperty.Register<LargeLabelControl, string>(
+        nameof(LargeText), defaultValue:"SMALL TEXT");
+
+    public string LargeText
+    {
+        get => GetValue(LargeTextProperty);
+        set => SetValue(LargeTextProperty, value);
+    }
+
+    public static readonly StyledProperty<string> SmallTextProperty = AvaloniaProperty.Register<LargeLabelControl, string>(
+        nameof(SmallText), defaultValue:"LARGE TEXT");
+
+    public string SmallText
+    {
+        get => GetValue(SmallTextProperty);
+        set => SetValue(SmallTextProperty, value);
+    }
+}
