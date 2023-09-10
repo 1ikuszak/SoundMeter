583b2c9955d7a42b02b26d5adc575a909940b46f
1ikuszak <luke.glica@gmail.com>
2023-09-10 02:28:28 +0200

    Popup btn content updates

--- SoundMeter/Styles/PopupControl.cs
+++ SoundMeter/Styles/PopupControl.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using SoundMeter.Services;
 using SoundMeter.ViewModels;
@@ -11,14 +12,6 @@ namespace SoundMeter
         {
             // Set the DataContext to the instance of this control (self)
             DataContext = new MainWindowViewModel(new DummyAudioInterfaceService());
-
-        }
-
-        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
-        {
-            base.OnApplyTemplate(e);
-
-            // Additional code for handling template changes if needed
         }
     }
 }
--- SoundMeter/ViewModels/MainWindowViewModel.cs
+++ SoundMeter/ViewModels/MainWindowViewModel.cs
@@ -2,13 +2,15 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia.Controls.Primitives;
 using ReactiveUI;
 using SoundMeter.DataModels;
 using SoundMeter.Services;
 
 namespace SoundMeter.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
         private List<IGrouping<string, ChannelConfigurationItem>> _channelConfigurations;
 
@@ -18,9 +20,24 @@ namespace SoundMeter.ViewModels
             set => this.RaiseAndSetIfChanged(ref _channelConfigurations, value);
         }
         
-
+        
+        private string _selectedChannel = "Select Channel"; // Initial value
+        public string SelectedChannel
+        {
+            get => _selectedChannel;
+            set => this.RaiseAndSetIfChanged(ref _selectedChannel, value);
+        }
+        
+        public ICommand ExpanderButtonClickCommand { get; }
+        
+        
         public MainWindowViewModel(IAudioInterfaceService audioInterfaceService)
         {
+            ExpanderButtonClickCommand = ReactiveCommand.Create<string>(text =>
+            {
+                // Update the selected channel
+                SelectedChannel = text;
+            });
             Initialize(audioInterfaceService);
         }
 
